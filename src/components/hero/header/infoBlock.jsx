e6a099d32824186811c1939b93e6c3827034123a
nikitaShaparenko <shaparenka_3@mail.ru>
2022-04-06 23:13:56 +0300

    dfg

--- src/components/hero/header/infoBlock.jsx
+++ src/components/hero/header/infoBlock.jsx
@@ -2,7 +2,7 @@
 function InfoBlock() {
   return (
     <div className="info-block border">
-      <div className="header fw-500 fs-50 col-white">Ledger</div>
+      <div className="header fw-600 fs-50 col-white">Ledger</div>
       <div className="description col-grey fw-400 fs-18">Описание</div>
     </div>
   );
