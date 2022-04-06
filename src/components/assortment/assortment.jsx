3ffe5382ffe3fac45791851870f0e34bdbf7b0f7
nikitaShaparenko <shaparenka_3@mail.ru>
2022-04-06 22:44:16 +0300

    features

--- src/components/assortment/assortment.jsx
+++ src/components/assortment/assortment.jsx
@@ -1,5 +1,31 @@
+import { products } from "../../data";
+import Card from "../card/card";
+import "./assortment.scss";
+
 function Assortment() {
-  return <div>Assortment</div>;
+  return (
+    <div className="assortment">
+      <div className="info-block">
+        <div className="header fs-48 col-white col-white">Наш Ассортимент</div>
+        <div className="description fs-18 fw-500 col-grey">
+          Списко товаро куотрые вы можете заказть
+        </div>
+      </div>
+      <div className="product-list">
+        {products.map((product, index) => {
+          return (
+            <Card
+              key={index}
+              name={product.name}
+              logo={product.logo}
+              description={product.description}
+              cost={product.cost}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Assortment;
