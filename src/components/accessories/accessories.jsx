ee344400d2ca89eaaaf63d644e10a6380b1f84fe
nikitaShaparenko <shaparenka_3@mail.ru>
2022-04-07 00:21:59 +0300

    sdf

--- src/components/accessories/accessories.jsx
+++ src/components/accessories/accessories.jsx
@@ -1,5 +1,27 @@
+import { accesories } from "../../data";
+import Card from "../card/card";
+import "./accessories.scss";
+
 function Accessories() {
-  return <div className="col-white">accessories</div>;
+  return (
+    <div className="accessories border">
+      <div className="border descritpion">sadfasdf</div>
+      <div className="border accessories-list">
+        {accesories.map((product, index) => {
+          return (
+            <Card
+              type="accessory"
+              key={index}
+              name={product.name}
+              logo={product.logo}
+              description={product.description}
+              cost={product.cost}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Accessories;
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,22 +1,45 @@
 import "./card.scss";
 
-function Card({name,logo, cost, description}) {
+function Card({ type, name, logo, cost, description }) {
+  let cardHeight, cardWidth, my, mx, cardBodyHeight, cardBodyWidth;
+  if (type === "accessory") {
+    cardHeight = 284;
+    cardWidth = 288;
+    cardBodyHeight = 228;
+    cardBodyWidth = 245;
+    my = 28;
+    mx = 21;
+  } else {
+    my = 33;
+    mx = 33;
+    cardHeight = 294;
+    cardWidth = 392;
+    cardBodyHeight = 248;
+    cardBodyWidth = 326;
+  }
+
   return (
-    <div className="card border">
-      <div className="content border">
-          <div className="item-name border col-white">
-              <div>{logo}</div>
-              <div className="price border">
-                  <div className="fw-500 fs-24 col-grey">{name}</div>
-                  <div className="fw-400 fs-18 col-dark-orange">{cost}</div>
-              </div>
+    <div
+      style={{ width: `${cardWidth}px`, height: `${cardHeight}` }}
+      className="card border"
+    >
+      <div
+        style={{
+          margin: `${mx}px ${my}px`,
+          width: `${cardBodyWidth}px`,
+          height: `${cardBodyHeight}`,
+        }}
+        className="content border"
+      >
+        <div className="item-name border `` col-white">
+          <div>{logo}</div>
+          <div className="price border">
+            <div className="fw-500 fs-24 col-grey">{name}</div>
+            <div className="fw-400 fs-18 col-dark-orange">{cost}</div>
           </div>
-          <div className="description fs-16px fw-400 col-grey">
-             {description}
-          </div>
-          <div className="btn border col-grey fw-600 fs-24">
-              Подробнее
-          </div>
+        </div>
+        <div className="description fs-16px fw-400 col-grey">{description}</div>
+        <div className="btn border col-grey fw-600 fs-24">Подробнее</div>
       </div>
     </div>
   );
