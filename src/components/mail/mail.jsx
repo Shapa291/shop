64501ccb21c413fa2e514658f50ff2e852c43222
nikitaShaparenko <shaparenka_3@mail.ru>
2022-04-08 18:17:28 +0300

    sfdsaf

--- src/components/mail/mail.jsx
+++ src/components/mail/mail.jsx
@@ -5,20 +5,32 @@ import pochta from "../../assets/pochta.png";
 import dhl from "../../assets/dhl.svg";
 import wildberries from "../../assets/wildberries.png";
 import ozon from "../../assets/ozon.png";
-import { mailLogo } from "../../data";
 
 function Mail() {
   return (
     <div className="mail container">
       <div className="col-hard-white fs-24 fw-500 content">
         Доставляем с помощью
-        {mailLogo.map((logo, index) => {
-          return (
-            <div className={`${logo}`}>
-              <img alt="logo" src={require(`../../assets/${logo}.png`)} />
-            </div>
-          );
-        })}
+        <div className="services">
+          <div className="evropochta">
+            <img src={evropochta} />
+          </div>
+          <div>
+            <img src={sdek} />
+          </div>
+          <div>
+            <img src={ozon} />
+          </div>
+          <div>
+            <img src={dhl} />
+          </div>
+          <div>
+            <img src={wildberries} />
+          </div>
+          <div>
+            <img src={pochta} />
+          </div>
+        </div>
       </div>
     </div>
   );
