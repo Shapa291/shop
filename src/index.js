cc79e803732acd9b26d8d652610119161426e129
nikitaShaparenko <shaparenka_3@mail.ru>
2022-04-09 13:55:32 +0300

    dsf

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/main.scss";
 import "./styles/zero-styles.scss";
+import "./styles/adaptive.scss"
 
 ReactDOM.render(
   <React.StrictMode>
