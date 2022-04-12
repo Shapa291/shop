567876d1924fbdf516bc87cadefe98b27851e53a
nikitaShaparenko <shaparenka_3@mail.ru>
2022-04-12 17:31:40 +0300

    mail logos

--- src/components/mail/mail.jsx
+++ src/components/mail/mail.jsx
@@ -1,4 +1,10 @@
 import "./mail.scss";
+import evropochta from "../../assets/mail/evropochta.png";
+import sdek from "../../assets/mail/sdek.png";
+import pochta from "../../assets/mail/pochta.png";
+import dhl from "../../assets/mail/dhl.png";
+import wildberries from "../../assets/mail/wildberries.png";
+import ozon from "../../assets/mail/ozon.png";
 import { mailLogo } from "../../data";
 
 function Mail() {
@@ -12,11 +18,29 @@ function Mail() {
               <div key={index} className={logo}>
                 <img
                   alt="mail-logo"
-                  src={require(`../../assets/mail/${logo}.png`)}
+                  src={`../../assets/mail/${logo}.png`}
                 />
               </div>
             );
           })}
+          {/* <div className="evropochta">
+            <img alt="mail" src={evropochta} />
+          </div>
+          <div>
+            <img alt="mail" src={sdek} />
+          </div>
+          <div>
+            <img alt="mail" src={ozon} />
+          </div>
+          <div>
+            <img alt="mail" src={dhl} />
+          </div>
+          <div>
+            <img alt="mail" src={wildberries} />
+          </div>
+          <div>
+            <img alt="mail" src={pochta} />
+          </div> */}
         </div>
       </div>
     </div>
