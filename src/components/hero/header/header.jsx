cb4f3a3f8a7004831410cf0ddd52d8851ad4bb3f
nikitaShaparenko <shaparenka_3@mail.ru>
2022-04-12 17:40:07 +0300

    Fixed navigation

--- src/components/hero/header/header.jsx
+++ src/components/hero/header/header.jsx
@@ -1,6 +1,6 @@
 import { refsOPtions } from "../../../data";
 import ledgerLogo from "../../../assets/ledgersLogo/ledger-logo-long.png";
-import {clickActions} from "../../../data";
+import { clickActions } from "../../../data";
 
 function Header() {
   return (
@@ -8,11 +8,15 @@ function Header() {
       <div className="content">
         <div className="wrapper">
           <div className="logo fs-22 fw-600">
-            <img alt="ledger-logo" src={ledgerLogo} />
+            <a href="#heroAnchor">
+              <img alt="ledger-logo" src={ledgerLogo} />
+            </a>
           </div>
           <ul className="refs fs-16px fw-500 col-grey">
             {refsOPtions.map((el, index) => (
-              <li key={index} ><a href={clickActions[index]}>{el}</a></li>
+              <li key={index}>
+                <a href={clickActions[index]}>{el}</a>
+              </li>
             ))}
           </ul>
         </div>
